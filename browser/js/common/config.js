5931c935d26a9e2cad0d15e539c151844839cfa4
Rachel Charow <rachel.charow@gmail.com>
2015-06-09 14:12:56 -0400

    Start carousel, fix gulp errors

--- browser/js/common/config.js
+++ browser/js/common/config.js
@@ -1,26 +1,26 @@
-app.config(function($stateProvider,$locationProvider,$urlRouterProvider) {
-	$stateProvider.state('browse',{
-		url: '/',
-		templateUrl: '/js/common/templates/home.html'//,
-		// controller:
-	})
-	$stateProvider.state('cart',{
-		url: '/cart',
-		templateUrl: '/js/common/templates/cart.html'//,
-		// controller:
-	})
-	$stateProvider.state('productDetail',{
-		url: '/product',
-		templateUrl: '/js/common/templates/product.html'//,
-		// controller:
-	})
+// app.config(function($stateProvider,$locationProvider,$urlRouterProvider) {
+// 	$stateProvider.state('browse',{
+// 		url: '/',
+// 		templateUrl: '/js/common/templates/home.html'//,
+// 		// controller:
+// 	})
+// 	$stateProvider.state('cart',{
+// 		url: '/cart',
+// 		templateUrl: '/js/common/templates/cart.html'//,
+// 		// controller:
+// 	})
+// 	$stateProvider.state('productDetail',{
+// 		url: '/product',
+// 		templateUrl: '/js/common/templates/product.html'//,
+// 		// controller:
+// 	})
 	
-	$stateProvider.state('cart.checkout',{
-		url: '/js/cart/checkout',
-		templateUrl: 'js/common/templates/checkout.html'//,
-		// controller:
-	})
+// 	$stateProvider.state('cart.checkout',{
+// 		url: '/js/cart/checkout',
+// 		templateUrl: 'js/common/templates/checkout.html'//,
+// 		// controller:
+// 	})
 	
-	$urlRouterProvider.otherwise('/')
+// 	// $urlRouterProvider.otherwise('/')
 	
-})
+// })
---
+++ browser/js/common/directives/carousel/carousel.js
@@ -0,0 +1,9 @@
+app.directive('carousel',function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'js/common/directvies/carousel/carousel.html',
+		link: function (scope){
+			
+		}
+	}
+})
