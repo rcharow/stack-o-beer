2cca82caad7e775d05425eab35e20cbe9c4cc454
Rachel Charow <rachel.charow@gmail.com>
2015-06-09 18:25:06 -0400

    Write more beer routes. Start testing beer routes

--- server/app/routes/beer/beerRoute.js
+++ server/app/routes/beer/beerRoute.js
@@ -2,17 +2,65 @@
 var router = require('express').Router();
 module.exports = router;
 var _ = require('lodash');
-var beer = require('Beer')
-var category = require('category')
+var mongoose = require('mongoose')
+// require('../../../server/db/models/beer');
+var Beer = mongoose.model('Beer')
+var Category = mongoose.model('Category')
+
+function getObjectParams(paramObj){
+	var params = []
+	for(var key in paramObj){
+		params.push(paramObj[key])
+	}
+
+	return params
+}
 
 router.get('/categories', function(req, res){
 	console.log('YOOOOOOOOO');
 
-	return category.find().exec().then(function(cats){
-		console.log(cats);
+	Category.find().exec().then(function(cats){
+		console.log('cats',cats);
 		res.json(cats)
 	},function(err){
 		res.status(500).end()
 	})
 
 })
+
+router.get('/categories/:categoryId',function(req,res){
+	var params = getObjectParams(req.params.categoryId)
+
+	Beer.find({cat_id: { $in: params } })
+	.exec()
+	.then(function(beers){
+		res.json(beers)
+	},function(err){
+		res.status(500).end()
+	})
+})
+
+router.get('/style/:styleId', function (req,res){
+	var params = getObjectParams(req.params.styleId)
+
+	Beer.find({style_id: { $in: params } })
+	.exec()
+	.then(function(beers){
+		res.json(beers)
+	},function(err){
+		res.status(500).end()
+	})
+})
+
+router.get('/:beerId',function (req,res){
+	Beer.findOne({_id: req.params.beerId})
+	.exec()
+	.then(function(beer){
+		console.log("BEER",beer)
+		res.json(beer)
+	},function(err){
+		res.status(500).end()
+	})
+})
+
+module.exports = router
--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -4,6 +4,8 @@ module.exports = router;
 
 router.use('/members', require('./members'));
 
+router.use('/beer', require('./beer/beerRoute'))
+
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
