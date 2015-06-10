dba0987bd11094b5a9ccb9c32263c400bf20d3a1
Stan Le <stan.le1107@gmail.com>
2015-06-10 13:54:10 -0700

    Fixed

--- server/db/models/brewery.js
+++ server/db/models/brewery.js
@@ -6,7 +6,7 @@ var schema = new mongoose.Schema({
 	city: {type: String, required: true},
 	state: {type: String, required: true},
 	website: {type:String, required:true},
-	rating: {type: Number, required: true, default: 0, min: 1, max 10},
+	rating: {type: Number, required: true, default: 0, min: 1, max: 10},
 	id: {type: Number, required: true}
 })
 
