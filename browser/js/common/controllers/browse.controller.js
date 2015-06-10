8d63c29f6f0c7e4f40019f5d6a0710fc5c783285
Rachel Charow <rachel.charow@gmail.com>
2015-06-10 18:51:18 -0400

    Add browse-list and browse-item directives. Start styling browse-item

---
+++ browser/js/common/controllers/browse.controller.js
@@ -0,0 +1,3 @@
+app.controller('BrowseController',function ($scope){
+
+})
--- browser/js/common/directives/browse-list/browse-list.js
+++ browser/js/common/directives/browse-list/browse-list.js
@@ -1,7 +1,7 @@
 app.directive('browselist',function(){
 	return {
 		restrict: 'E',
-		templateUrl: 'js/common/directvies/beer-list/browse-list.html',
+		templateUrl: 'js/common/directives/browse-list/browse-list.html',
 		link: function (scope){
 			
 		}
