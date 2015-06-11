850088357eb7e859c28b25c12a126a5a444cee88
Rachel Charow <rachel.charow@gmail.com>
2015-06-10 20:49:22 -0400

    Add script for adding cat_oid to beer

---
+++ browser/js/common/controllers/browse.controller.js
@@ -0,0 +1,15 @@
+app.controller('BrowseController',function ($scope){
+	$scope.beers = 
+	[
+		{
+			category: 'Lager',
+				beers: [
+					{
+						beer_name:,
+						brewery:,
+						descript:
+					}
+				]	
+		}
+	]
+})
