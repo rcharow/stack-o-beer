aaa73c5fd5b8eccc0ee02eb657db4e58d349361e
Rachel Charow <rachel.charow@gmail.com>
2015-06-12 11:50:20 -0400

    Filter out empty categories. Automatically load first category.

--- browser/js/common/controllers/main.controller.js
+++ browser/js/common/controllers/main.controller.js
@@ -1,26 +1,93 @@
-app.controller('MainController',function($scope,$modal,DisplayBeerFactory,SideBarFactory){
-	SideBarFactory.getBeerCategories().then(function(categories){
+app.controller('MainController',function($scope, SideBarFactory, DisplayBeerFactory, UpdateCart, AuthService){
+		console.log("IN Main CONTROLLER");
+		
+		var cart = [];
 
-		$scope.categories = categories;
+		SideBarFactory.getBeerCategories().then(function(categories){
 
-	})
+			$scope.categories = categories;
 
-	$scope.goToCategory = function(categoryID){
-		DisplayBeerFactory.getBeerByCategory(categoryID).then(function(beers){
-
-			$scope.beers = beers
-		})
-	}
-
-	$scope.displayBeer = function (beer){
-		var modalProduct = $modal.open({
-			animation: $scope.animationEnabled,
-			templateUrl: '/js/common/templates/productDetails.html',
-			controller: 'ProductController',
-			size: 'lg',
-			resolve: { 
-				beer:function(){return beer}
+			if(!$scope.beers){
+				$scope.goToCategory($scope.categories[0]._id)
 			}
+
+			console.log('In categoryController', categories);
 		})
-	}
+
+		$scope.goToCategory= function(categoryID){
+			DisplayBeerFactory.getBeerByCategory(categoryID).then(function(beers){
+
+				$scope.beers= beers
+				return
+			})
+		}
+
+
+
+
+		$scope.displayBeer = function (beer){
+				var modalProduct = $modal.open({
+					animation: $scope.animationEnabled,
+					templateUrl: '/js/common/templates/productDetails.html',
+					controller: 'ProductController',
+					size: 'lg',
+					resolve: { 
+						beer:function(){return beer}
+					}
+				})
+			}
+
+
+
+
+		var loggedInUser;
+
+		AuthService.getLoggedInUser().then(function (user){
+
+			if(user)
+				{	console.log('YOOOOOOOOOO is' , user)
+					loggedInUser= user;
+					
+				}
+
+		})
+
+
+
+
+
+		
+		$scope.buyBeer= function(i){
+			var stuff = {
+				item: 'Hey',
+				price: 2,
+				qty: 3
+			}
+
+			cart.push(stuff);
+			var jsonStr = JSON.stringify(cart);
+
+			sessionStorage.setItem("test", jsonStr);
+
+			var item = sessionStorage.getItem("test");
+			console.log(item)
+
+			if (loggedInUser)
+				{
+
+					var itemObj = JSON.parse(item);
+					console.log('Itemobj is', itemObj);
+					UpdateCart.insertItem(itemObj,loggedInUser).then(function(){
+						console.log('inserted')
+					})
+			}
+
+			else
+			{
+				var guest = sessionStorage.getItem("test");
+				console.log(guest);
+			}
+			
+
+		}
 })
--- server/app/routes/beer/categoryRoute.js
+++ server/app/routes/beer/categoryRoute.js
@@ -5,15 +5,18 @@ var _ = require('lodash');
 var mongoose = require('mongoose')
 var Beer = mongoose.model('Beer')
 var Category = mongoose.model('Category')
+var Q = require('q')
 
 module.exports = router
 
 router.get('/', function (req, res, next){
-	
-	Category.find().exec().then(function(cats){
-		console.log('cats',cats);
-		res.json(cats)
-	},next)
-	
-
+	Beer.find()
+	.distinct('cat_oid',function(err,catIds){
+		console.log(catIds)
+		Category.find({_id: { $in: catIds } })
+		.exec()
+		.then(function(cats){
+			res.json(cats)
+		},next)
+	})
 })
