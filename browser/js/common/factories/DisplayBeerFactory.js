3b4af2e20123b1f153495bf44f78054b45a21542
Rachel Charow <rachel.charow@gmail.com>
2015-06-12 10:51:50 -0400

    Change image size

--- browser/js/common/factories/DisplayBeerFactory.js
+++ browser/js/common/factories/DisplayBeerFactory.js
@@ -1,13 +1,7 @@
 app.factory('DisplayBeerFactory', function($http){
 
 	return {
-		// getTypeBeer: function(category){
 
-		// 	return $http.get('/api/categories/'+category).then(function(results)
-		// 	{
-		// 		console.log(results.data)
-		// 	})
-		// },
 		getBeerByCategory: function(id){
 			var query = {}
 			query.catId = id
